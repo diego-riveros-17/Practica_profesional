7c9d262a5dca6795b6ec05ebba3b8f42900e8c75
diego.rvs17 <diegoriveros1991@gmail.com>
2026-06-09 15:42:13 -0300

    feat: se inicio con la base del arreglo de usuarios

---
+++ assets/js/login.js
@@ -0,0 +1,25 @@
+let usuarios = [
+  {
+    id: 1,
+    nombre: "Diego",
+    apellido: "Riveros",
+    email: "diegoriveros1991@gmail.com",
+    dni: 36204965,
+    direccion: "B° Obreo, Salta 1250",
+    user: "",
+    password: "",
+    rol: "ciudadano",
+  },
+
+  {
+    id: 2,
+    nombre: "Francisco",
+    apellido: "Martinez",
+    email: "fran2mart5@gmail.com",
+    dni: 36204965,
+    direccion: "B° San Miguel, Carlos Brunelli 1536",
+    user: "",
+    password: "",
+    rol: "ciudadano",
+  },
+];
